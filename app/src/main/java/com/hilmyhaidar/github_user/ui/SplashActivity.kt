c6e6659ff1e16c0d51ad247560bfbcf7a8840d72
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2021-04-30 15:58:49 +0700

    ongoing splashscreen

---
+++ app/src/main/java/com/hilmyhaidar/github_user/ui/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.hilmyhaidar.github_user.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
