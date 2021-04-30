448d76e43887c06aa0ec5802c9c1c8443513c306
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2021-04-30 16:24:22 +0700

    splashscreen done

--- app/src/main/java/com/hilmyhaidar/github_user/ui/SplashActivity.kt
+++ app/src/main/java/com/hilmyhaidar/github_user/ui/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.hilmyhaidar.github_user.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.hilmyhaidar.github_user.databinding.ActivityMainBinding
+import com.hilmyhaidar.github_user.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
